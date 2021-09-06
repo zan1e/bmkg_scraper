af2dd777fb53bc556597dd3959e302359dbeac11
zaniesec <seczanie@gmail.com>
2021-09-06 16:55:09 +0900

    making requests to the website

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = "https://www.bmkg.go.id/cuaca/prakiraan-cuaca-indonesia.bmkg"
 
+URL = "https://www.bmkg.go.id/cuaca/prakiraan-cuaca-indonesia.bmkg"
 request = requests.get(URL)
+
+if request.status_code != 200:
+    print(request.status_code)
+    exit()
+
 
