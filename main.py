bdb7ada17f45eb5ff0f471def1f7426ce1cc269e
zaniesec <seczanie@gmail.com>
2021-09-06 17:11:09 +0900

    adding request to each location

--- main.py
+++ main.py
@@ -14,5 +14,7 @@ soup = BeautifulSoup(request.text,"lxml")
 locations = soup.select('div.row.list-cuaca-provinsi.md-margin-bottom-10 > div')
 
 for location in locations:
-    print(location.select_one('a').text)
+    request_location_page = requests.get(f"https://www.bmkg.go.id/cuaca/prakiraan-cuaca-indonesia.bmkg{location.select_one('a')['href']}")
+    print(location.select_one('a').text,end=" : ")
+    print(request_location_page.status_code)
 
