8b2fd0dfb8b5b6666fc3e74244be72a1be5f857d
zaniesec <seczanie@gmail.com>
2021-09-06 17:38:02 +0900

    iterating cities

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ if request.status_code != 200:
     print(request.status_code)
     exit()
 
-soup = BeautifulSoup(request.text,"lxml")
+main_soup = BeautifulSoup(request.text,"lxml")
 
-locations = soup.select('div.row.list-cuaca-provinsi.md-margin-bottom-10 > div')
+locations = main_soup.select('div.row.list-cuaca-provinsi.md-margin-bottom-10 > div')
 
 for location in locations:
     request_location_page = requests.get(f"https://www.bmkg.go.id/cuaca/prakiraan-cuaca-indonesia.bmkg{location.select_one('a')['href']}")
-    print(location.select_one('a').text,end=" : ")
-    print(request_location_page.status_code)
+    location_title = location.select_one('a').text
+    location_soup = BeautifulSoup(request_location_page.text,"lxml")
+    cities = location_soup.select_one('#TabPaneCuaca1 > div > table')
 
+    for city_name in cities.select('tr > td:nth-child(1) > a'):
+        print(city_name) 
+    break
