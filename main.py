ecfa0c8587d0788e3ad31f242c36be257d68dca4
zaniesec <seczanie@gmail.com>
2021-09-06 19:36:55 +0900

    scrape data for each city

--- main.py
+++ main.py
@@ -13,12 +13,20 @@ main_soup = BeautifulSoup(request.text,"lxml")
 
 locations = main_soup.select('div.row.list-cuaca-provinsi.md-margin-bottom-10 > div')
 
+i = 0
 for location in locations:
     request_location_page = requests.get(f"https://www.bmkg.go.id/cuaca/prakiraan-cuaca-indonesia.bmkg{location.select_one('a')['href']}")
     location_title = location.select_one('a').text
     location_soup = BeautifulSoup(request_location_page.text,"lxml")
-    cities = location_soup.select_one('#TabPaneCuaca1 > div > table')
+    location_table = location_soup.select_one('#TabPaneCuaca1 > div > table ')
+    cities = [location_table.select('tr > td > a')]
+    temp = [location_table.select('tr > td:nth-child(4)')]
+    humidity = [location_table.select('tr > td:nth-child(5)')]
+    date = location_soup.select_one('div.prakicu-kabkota.tab-v1 > ul > li:nth-child(1) > a').text
 
-    for city_name in cities.select('tr > td:nth-child(1) > a'):
-        print(city_name) 
-    break
+    print(f'{location_title} - {date}',end="\n\n")
+
+    for i in range(len(cities[0])):
+        print(cities[0][i].text)
+        print(temp[0][i].text)
+        print(humidity[0][i].text)
